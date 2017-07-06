6052286afd75d513084e655fd933020fcf1cd13a
Refe George <rgeorge@opentrends.net>
2017-07-06 14:43:48 +0530

    initial commit

---
+++ FirebasePOC/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  FirebasePOC
+//
+//  Created by Ann Mac on 03/07/17.
+//  Copyright © 2017 MACMINI 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import Firebase;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
